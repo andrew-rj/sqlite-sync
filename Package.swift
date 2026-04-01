34b3d70c92e42b482dd0771648301290f5c9336a
Gioee <Gioee@users.noreply.github.com>
2026-04-01 10:02:36 +0000

    Update Package.swift checksum for 1.0.5 [skip ci]

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "CloudSyncBinary",
-            url: "https://github.com/sqliteai/sqlite-sync/releases/download/1.0.3/cloudsync-apple-xcframework-1.0.3.zip",
-            checksum: "b2fba950d308dc004003774821ae8f5baa770c7b7d1c1189956e25537d348fe5"
+            url: "https://github.com/sqliteai/sqlite-sync/releases/download/1.0.5/cloudsync-apple-xcframework-1.0.5.zip",
+            checksum: "8efd5da9e69731f0ed32d0e137314e260c2c8d763c5dc07672092d642418364b"
         ),
         .target(
             name: "CloudSync",
