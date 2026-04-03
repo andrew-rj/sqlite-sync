7a3d1d55a9be28c26fad345bda57ad759a1ec26e
andinux <andinux@users.noreply.github.com>
2026-04-03 16:13:31 +0000

    Update Package.swift checksum for 1.0.8 [auto-update]

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "CloudSyncBinary",
-            url: "https://github.com/sqliteai/sqlite-sync/releases/download/1.0.7/cloudsync-apple-xcframework-1.0.7.zip",
-            checksum: "018696d0c84a0d69619d05904d276d1558ff6f812ac911854e9c62af6aa022c9"
+            url: "https://github.com/sqliteai/sqlite-sync/releases/download/1.0.8/cloudsync-apple-xcframework-1.0.8.zip",
+            checksum: "5a66dae8ca2b2a2af101685a0c177fa49705bc5080f70da967292c1360e712b0"
         ),
         .target(
             name: "CloudSync",
